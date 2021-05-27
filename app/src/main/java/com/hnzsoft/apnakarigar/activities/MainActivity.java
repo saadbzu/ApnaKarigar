8b8c59605edc2b3dec5d5ab29ac5446de031eb2c
ad_developer <saadansari633@gmail.com>
2021-05-27 19:52:22 +0500

    Phone Authuntication Work

--- app/src/main/java/com/hnzsoft/apnakarigar/activities/MainActivity.java
+++ app/src/main/java/com/hnzsoft/apnakarigar/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.hnzsoft.apnakarigar;
+package com.hnzsoft.apnakarigar.activities;
 
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +14,7 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.hnzsoft.apnakarigar.R;
 import com.hnzsoft.apnakarigar.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {

0369e321f2a255b482e314af2421bb7c75429174
ad_developer <saadansari633@gmail.com>
2021-05-27 19:57:54 +0500

    Firebase Phone Authticated Work

---
+++ app/src/main/java/com/hnzsoft/apnakarigar/activities/PhoneNumberActivity.java
@@ -0,0 +1,16 @@
+package com.hnzsoft.apnakarigar.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hnzsoft.apnakarigar.R;
+
+public class PhoneNumberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_number);
+    }
+}
